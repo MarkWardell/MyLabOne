05a5774aa9fc105fc000c4b14455c69eda1100bd
Mark Wardell <mark.d.wardell@avanade.com>
2018-09-11 07:21:13 -0500

    Put Them all In

---
+++ Solution/DoSomeConfig/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Configuration;
+namespace DoSomeConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strName = ConfigurationManager.AppSettings["name"];
+            string strAge  = ConfigurationManager.AppSettings["age"];
+            Console.WriteLine(String.Format("Hello {0} you are {1} years old",strName,strAge));
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ Solution/HelloCollecctions/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace HelloCollecctions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var listLang   = ConfigurationManager.AppSettings.AllKeys;
+            var ListValues = new List<String>();
+            for (int i = 0; i < listLang.Length; i++)
+            {
+                var str = ConfigurationManager.AppSettings.Get(i);            
+                ListValues.Add(str);
+            }
+
+
+         
+            listBox1.DataSource = ListValues;
+        }
+    }
+}
